dc5e0e04e0eb0f2274770459c6c27729f4471b4d
Yuliya1990 <84015911+Yuliya1990@users.noreply.github.com>
2021-12-03 21:35:15 +0200

    Add files via upload

---
+++ Solid3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+interface IFigure
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int GetArea();
+}
+
+class Rectangle : IFigure
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+//квадрат наслідується від прямокутника!!!
+class Square : IFigure
+{
+    private int width = 0;
+    private int height = 0;
+    public int Width { get { return width; } set { width = value; height = value; } }
+    public int Height { get { return height; } set { width = value; height = value; } }
+    public int GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IFigure sqr = new Square();
+        sqr.Width = 5;
+        Console.WriteLine(sqr.GetArea());
+        Console.ReadKey();
+    }
+}
