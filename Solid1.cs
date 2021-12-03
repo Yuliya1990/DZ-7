19dfff6c6604dcf0f08dc05c378354b03a9e7d62
Yuliya1990 <84015911+Yuliya1990@users.noreply.github.com>
2021-12-03 20:53:57 +0200

    Add files via upload

---
+++ Solid1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid1
+{
+    //Порушено принцип єдиного обов'язку (The single responsibility principle SRP)
+    class Item
+    {
+
+    }
+    class Order
+    {
+        private List<Item> itemList;
+
+        internal List<Item> ItemList
+        {
+            get
+            {
+                return itemList;
+            }
+
+            set
+            {
+                itemList = value;
+            }
+        }
+        
+        public void GetItems() {/*...*/}
+        public void GetItemCount() {/*...*/}
+        public void AddItem(Item item) {/*...*/}
+        public void DeleteItem(Item item) {/*...*/}
+        public void ShowOrder() {/*...*/}
+
+    }
+    public class Calculator
+    {
+        public void CalculateTotalSum() {/*...*/}
+    }
+
+    public class PrintManager
+    {
+        public void PrintOrder() {/*...*/}
+    }
+    public class OrderRepository
+    {
+        public void Load() {/*...*/}
+        public void Save() {/*...*/}
+        public void Update() {/*...*/}
+        public void Delete() {/*...*/}
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
